1e5a0dcee1891bfbbd8e5d7b2dcf52b1a911759e
kattunforever49 <kattunforever49@yahoo.com.tw>
2022-03-02 11:20:05 +0800

    First Upload

---
+++ src/settings.c8510.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","Game","UI"],collisionMatrix:[[true],[false,false],[false,false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/0.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",main:"e02af"}};
